ece9421a7eccb2174dac4ccf2cf25a85ad40b5cf
wa1tnr <wa1tnr@users.noreply.github.com>
2018-11-25 03:39:15 +0000

    On branch testing_aaa- 	modified:   job.fs

--- ktimer/job.fs
+++ ktimer/job.fs
@@ -1,4 +1,4 @@
-
+\ program
 variable aaa
 
 : somedelay ( -- )
@@ -14,23 +14,53 @@ variable aaa
          somedelay
        again ;
 
-: endless ( -- )
+: endlessthree ( -- ) \ best
     begin
-        largedelay
         aaa @ 1 + aaa !
         ." still in endless .."
         cr .s
         aaa @ .
     again ;
 
+
+: endless ( -- )
+    begin
+        largedelay
+    again ;
+
+
+
 : noending ( - )
   10 1 do 43 emit loop endless ;
 
+: timer_aa ( -- )
+
+  somedelay
+
+  aaa @ \ get variable
+  ." report aaa: "
+  dup .
+
+  1 + dup \ add 1 to int value stored at aaa and copy it for this test:
+  \ ( aaa -- aaa 1 -- aaa+1 -- aaa+1 aaa+1 )
+  59 > if \ ( aaa+1 aaa+1 -- a1 a1 59 -- a1 BOOL -- a1 )
+      drop \ ( a1 -- )
+      0    \ ( -- 0 )
+      aaa ! \ ( 0 -- ) aaa: aaa=0
+      cr exit then
+  aaa !
+  ." end of timer_aa: " .s cr
+;
+
 : herefuu ( - )
   -99 -98 -97
+
+  0 aaa ! \ reset timer
+
+  70 1 do timer_aa aaa @ . loop
   noending
 ;
 
 : go herefuu ;
 
-
+\ end
